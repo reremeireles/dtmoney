a4bd5d9ea2d40803f9f8e4f48800897c8c6804f7
Renata Meireles <renatameirelesbatista@gmail.com>
2023-02-02 19:27:29 -0300

    Add backend production server

--- src/index.tsx
+++ src/index.tsx
@@ -3,47 +3,49 @@ import ReactDOM from 'react-dom/client';
 import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
-createServer({
-  models: {
-    transaction: Model,
-  },
+if (process.env.NODE_ENV === 'development') {
+  createServer({
+    models: {
+      transaction: Model,
+    },
 
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Mercado',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 500,
-          createdAt: new Date('2022-09-19 11:54:00'),
-        },
-        {
-          id: 2,
-          title: 'Viagem',
-          type: 'deposit',
-          category: 'Pessoal',
-          amount: 250,
-          createdAt: new Date('2022-09-19 11:54:01'),
-        },
-      ]
-    })
-  },
+    seeds(server) {
+      server.db.loadData({
+        transactions: [
+          {
+            id: 1,
+            title: 'Mercado',
+            type: 'withdraw',
+            category: 'Casa',
+            amount: 500,
+            createdAt: new Date('2022-09-19 11:54:00'),
+          },
+          {
+            id: 2,
+            title: 'Viagem',
+            type: 'deposit',
+            category: 'Pessoal',
+            amount: 250,
+            createdAt: new Date('2022-09-19 11:54:01'),
+          },
+        ]
+      })
+    },
 
-  routes() {
-    this.namespace = 'api';
+    routes() {
+      this.namespace = 'api';
 
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
+      this.get('/transactions', () => {
+        return this.schema.all('transaction')
+      })
+      this.post('/transactions', (schema, request) => {
+        const data = JSON.parse(request.requestBody)
 
-      return schema.create('transaction', data)
-    })
-  },
-})
+        return schema.create('transaction', data)
+      })
+    },
+  })
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
