3ff89008415516a91393e87fe187f24c499141a1
Renata Meireles <renatameirelesbatista@gmail.com>
2023-02-02 21:45:50 -0300

    Setting Environment Variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createServer, Model } from 'miragejs';
 import { App } from './App';
-
+console.log(process.env.API_URL)
 if (process.env.NODE_ENV === 'development') {
   createServer({
     models: {
